231406f8657cb1068d8eb0e1f9a52901c432a119
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2025-06-16 21:56:41 +0300

    refactor index.php - added error, exception handlers, setup autoloading,setup autoloading, setup database to use ENV vars, added routing

--- public/index.php
+++ public/index.php
@@ -2,13 +2,26 @@
 
 declare(strict_types=1);
 
-header("content-type: application/json; charset=UTF-8");
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-require __DIR__ . "/Router.php";
+set_error_handler("App\\ErrorHandler::handleError");
+set_exception_handler("App\\ErrorHandler::handleException");
 
-$router = new Router();
-$router->add("POST", "/api/register", function () {
-  echo json_encode(["message" => "User registration endpoint !"]);
-});
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+header("Content-type: application/json; charset=UTF-8");
+
+$database = new App\Database(
+  $_ENV['DB_HOST'],
+  $_ENV['DB_NAME'],
+  $_ENV['DB_USER'],
+  $_ENV['DB_PASSWORD']
+);
+
+$router = new App\Router();
+
+$user_controller = new App\Controllers\UserController($database);
+$router->add("POST", "/api/register", [$user_controller, "register"]);
 
 $router->dispatch();
