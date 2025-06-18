4513ebc6ac307beb9990f77940ea029d00105c48
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2025-06-18 22:30:47 +0300

    Documented index.php, Validator.php Registered a new route (login) in index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,23 @@
 <?php
 
+// Enable strict types
 declare(strict_types=1);
 
+// Load composers autoloader
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+// Setup global error / exception handlers
 set_error_handler("App\\ErrorHandler::handleError");
 set_exception_handler("App\\ErrorHandler::handleException");
 
+// Load Environmental variables
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
+// Set default Content-Type header
 header("Content-type: application/json; charset=UTF-8");
 
+// Initialize Core Services
 $database = new App\Database(
   $_ENV['DB_HOST'],
   $_ENV['DB_NAME'],
@@ -21,7 +27,14 @@ $database = new App\Database(
 
 $router = new App\Router();
 
+// Define API routes
 $user_controller = new App\Controllers\UserController($database);
+
+// Registration Route
 $router->add("POST", "/api/register", [$user_controller, "register"]);
 
+// Login route
+$router->add("POST", "/api/login", [$user_controller, "login"]);
+
+// Dispatch the request
 $router->dispatch();
--- src/Services/Validator.php
+++ src/Services/Validator.php
@@ -13,22 +13,30 @@ class Validator
 
   public function validateRegistration(array $data): array
   {
+    // Initialize the errors array
     $errors = [];
 
+    // Verify if name has value
     if (empty($data["name"])) {
       $errors[] = "Name is required.";
     }
+
+    // Verify if email has value
     if (empty($data["email"])) {
       $errors[] = "Email is required.";
     }
+
+    // Verify if password has value
     if (empty($data["password"])) {
       $errors[] = "Password is required.";
     }
 
+    // Email format verification
     if (isset($data["email"]) && !filter_var($data["email"], FILTER_VALIDATE_EMAIL)) {
        $errors[] = "Invalid email format.";
     }
 
+    // Password validation
     if (isset($data["password"])) {
       if (strlen($data["password"]) < 5) {
         $errors[] = "Password must be at least 5 characters long.";
