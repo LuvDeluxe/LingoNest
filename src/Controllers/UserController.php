92b4d656008825db9a42057c5d5aa705e9f534cf
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2025-06-19 10:28:00 +0300

    Added method handling login for users

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -67,4 +67,38 @@ class UserController
     echo json_encode(["message" => "User registration endpoint reached successfully",
       "user_id" => $this->conn->lastInsertId()]);
   }
+
+  /**
+   * Handles the user login process.
+   */
+  public function login(): void
+  {
+    // Get input data
+    $data = (array) json_decode(file_get_contents("php://input"), true);
+
+    // Validate
+    if (!isset($data["email"]) || !isset($data["password"])) {
+      http_response_code(422);
+      echo json_encode(["message" => "Missing email or password"]);
+      return;
+    }
+
+    // Find user by email
+    $sql = "SELECT * FROM users WHERE email = :email";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->bindValue(":email", $data["email"], PDO::PARAM_STR);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Verify User and password
+    if ($user === false || !password_verify($data["password"], $user["password"])) {
+      http_response_code(401);
+      echo json_encode(["message" => "Invalid credentials."]);
+      return;
+    }
+
+    // Authentication successful
+    echo json_encode(["message" => "Login successful", "user_id" => $user["id"]]);
+  }
 }
